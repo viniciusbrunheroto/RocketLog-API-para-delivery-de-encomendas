860999e26c36b9d39ec91c372fb8c40006fb4443
Vinícius Brunheroto <vinibrunheroto12@gmail.com>
2026-07-10 19:42:51 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,6 @@ import { env } from "./env";
 
 const PORT = env.PORT
 
-console.log(process.env.PORT)
-console.log(env)
-
 app.listen(PORT, () => {
     console.log(`HTTP Server running on port ${PORT}`)
 })
